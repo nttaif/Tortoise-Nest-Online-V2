9b3b22cddd3101f5c44f4afb1e01b0576f767fae
nttaif <nt.tai5304@gmail.com>
2025-01-09 02:35:56 +0700

    feat: enhance application setup with Swagger integration and global prefix configuration

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,16 +3,36 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllConfigType } from './config/config.type';
 import { ConfigService } from '@nestjs/config';
+import { useContainer } from 'class-validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  app.enableShutdownHooks();
   const configService = app.get(ConfigService<AllConfigType>);
+  app.setGlobalPrefix(
+    configService.getOrThrow('app.apiPrefix', { infer: true }),
+    {
+      exclude: ['/'],
+    },
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
       forbidNonWhitelisted: true,
-    })
+    }),
   );
+  //set up swagger
+  const options = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API docs')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(configService.getOrThrow('app.port', { infer: true }));
 }
 bootstrap();
