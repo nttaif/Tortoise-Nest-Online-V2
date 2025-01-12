863b33ff32b0ea89cd060fba1e5e992ce0845d6e
Vũ Trường <truongvunguyen3434@gmail.com>
2025-01-12 15:58:49 +0700

    docs: add detailed documentation for password comparison utility function

--- server/src/util/compare.password.ts
+++ server/src/util/compare.password.ts
@@ -1,5 +1,15 @@
 import bcrypt from 'bcrypt';
 
+
+/**
+ * 
+ * @param password 
+ * @param rePassword
+ * Step 1 : compare password and rePassword
+ * Step 2 : If !isMatch return false
+ * Step 3 : else return true
+ * @returns true or false
+ */
 export const checkPassword = async (password: string, rePassword: string) => {
     try {
         const isMatch = await bcrypt.compare(password,rePassword);
