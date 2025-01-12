95ab2b0988ff99bdce721d3c931817aca2633f05
nttaif <nt.tai5304@gmail.com>
2025-01-12 10:55:57 +0700

    refactor: update import path for useAuth hook in DashboardForm component

--- client/src/features/admin/component/DashboardForm.tsx
+++ client/src/features/admin/component/DashboardForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useAuth } from '../hook/useAuth';
+import { useAuth } from '@/features/auth/hook/useAuth';
 const LoginPage = () => {
   const { login, user, loading, error } = useAuth();
   const [email, setEmail] = useState<string>('');
