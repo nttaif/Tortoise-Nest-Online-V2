482a3ace086dab1ae38a6359e1109120d313aac3
nhan080804 <nhan080804@gmail.com>
2025-01-25 00:05:13 +0700

    feat: implement login page with form components and styling

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,7 +1,12 @@
+import LoginFormComponent from '@/components/auth/login.form.component'
 import React from 'react'
 
 export default function login() {
   return (
-    <div>login</div>
+    <div className="flex min-h-screen items-center justify-center bg-gray-900">
+       <div className="w-full max-w-lg  bg-gray-800 p-8 rounded-md shadow-md text-white">
+          <LoginFormComponent></LoginFormComponent>
+        </div>
+    </div>
   )
 }
--- client/src/components/auth/login.form.component.tsx
+++ client/src/components/auth/login.form.component.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+import { Checkbox } from '../ui/checkbox'
+import FormInfouserComponents from './form.infouser.components'
+import FormInfoUserComponents from './form.infouser.components'
 
 export default function LoginFormComponent() {
   return (
-    <div>login.form.component</div>
+    <div className="w-full max-w-md bg-gray-800 p-8 rounded-md shadow-md text-white">
+      <h1 className="text-2xl text-white mb-8 font-bold ">Welcome back TNO </h1>
+      <FormInfoUserComponents></FormInfoUserComponents>
+      <div className='mt-10' >
+      <p className="text-center text-gray-400 mb-6">
+        Start your website in seconds. Don’t have an account?{" "}
+        <a href="#" className="text-blue-500 hover:underline">
+          Sign up
+        </a>
+      </p>
+      </div>
+    </div>
   )
 }
