5bc51b2485e25d32864f911f298b26515de2ae63
Nhan <nhan080804@gmail.com>
2025-02-15 12:09:25 +0700

    Register form working 50%

--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -1,7 +1,12 @@
+import FormRegisterComponents from '@/components/auth/register/form.register.components'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>Register</div>
+    <div className="flex min-h-screen items-center justify-center bg-gray-900">
+           <div className="w-full max-w-lg bg-gray-800 p-8 rounded-md shadow-md text-white outline outline-1 outline-gray-700">
+              <FormRegisterComponents></FormRegisterComponents>
+            </div>
+        </div>
   )
 }
